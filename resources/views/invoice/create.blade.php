b65fdf9de1115c9c663868998f3525f5da4d3614
Kashif <ahamed.kashif@northsouth.edu>
2022-05-30 00:33:55 +0600

    ui chng

--- resources/views/invoice/create.blade.php
+++ resources/views/invoice/create.blade.php
@@ -10,12 +10,12 @@
             <form action="{{route('invoice.initial')}}" method="post">
                 @csrf
                 <input type="hidden" name="is_checkin" value="1" />
-                <button type="submit" class="btn btn-outline-dark btn-lg btn-block waves-effect waves-light mb-1">Credit</button>
+                <button type="submit" class="btn btn-outline-dark btn-lg btn-block waves-effect waves-light mb-1">Receive</button>
             </form>
             <form action="{{route('invoice.initial')}}" method="post">
                 @csrf
                 <input type="hidden" name="is_checkin" value="0" />
-                <button type="submit" class="btn btn-outline-dark btn-lg btn-block waves-effect waves-light mb-1">Debit</button>
+                <button type="submit" class="btn btn-outline-dark btn-lg btn-block waves-effect waves-light mb-1">Expense</button>
             </form>
         </div>
 @endsection
