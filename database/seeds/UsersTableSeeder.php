54f6cab875df5b756c32bd22c7d627b8fa55c422
Kashif <ahamed.kashif@northsouth.edu>
2022-08-10 21:51:12 +0600

    migration added

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,36 +15,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //Add root user
-        $user = User::create(
-            [
-                'id' 		=> 	1,
-                'name'	=>	'Dev',
-                'email'		=>	'sahkashif@gmail.com',
-                'phone_number' => '01989091217',
-                'password'	=>	Hash::make('12345678')
-            ]
-        );
-        //Add root user
-        $user = User::create(
-            [
-                'id' 		=> 	2,
-                'name'	=>	'Ariful Islam',
-                'email'		=>	'account@fsrel.com',
-                'phone_number' => '01989091218',
-                'password'	=>	Hash::make('12345678')
-            ]
-        );
         $role = Spatie\Permission\Models\Role::create(['name' => 'super-admin']);
         $role->givePermissionTo(Spatie\Permission\Models\Permission::all());
 
         $role = Spatie\Permission\Models\Role::create(['name' => 'admin']);
         $role->givePermissionTo(Spatie\Permission\Models\Permission::all());
 
-        $root_user = User::find(1);
-        $root_user->assignRole('super-admin');
-
-        $admin_user = User::find(2);
-        $admin_user->assignRole('admin');
+        $role = Spatie\Permission\Models\Role::create(['name' => 'manager']);
+        $role->givePermissionTo(Spatie\Permission\Models\Permission::whereNotIn('name',['delete project','delete invoice','delete customer','create salary','delete employee','create employee','index employee','show employee','update employee','api_get employee'])->get());
+        //Add root user
+        $user = User::findOrFail(1)->assignRole('super-admin');
+        $user = User::findOrFail(2)->assignRole('admin');
+        $user = User::findOrFail(3)->assignRole('manager');
+        //Add root user
     }
 }
